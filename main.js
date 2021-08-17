c0734b7a96292b8fb70147008669c5e0b92444c6
H4r1eyDev <85908279+H4r1eyDev@users.noreply.github.com>
2021-08-17 18:27:09 +0100

    Update main.js
    Fixed some issues and cleaned up the config. Embeds now also look alot better and not all over the place. I will be re-updating this to be alot better in the future.

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const Discord = require('discord.js'); // V12
-
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 client.config = require("./config.json");
-
 const fs = require('fs');
+let prefix = client.config["mainConfig"].botPrefix
 
-let prefix = client.config["main_config"].prefix
 
 client.commands = new Discord.Collection();
 
@@ -17,12 +15,12 @@ for(const file of commandFiles){
 }
 
 client.once('ready', () =>{
-	console.log(`Bot Online! Logged in as ${client.user.tag}. Using The Prefix ${client.config["main_config"].prefix}`);
-	client.user.setActivity(client.config["main_config"].botstatus, { type: client.config["main_config"].presence })
+	console.log(`Bot Online! Logged in as ${client.user.tag}. Using The Prefix ${client.config["mainConfig"].botPrefix}`);
+	client.user.setActivity(client.config["mainConfig"].botStatus, { type: client.config["mainConfig"].botPresence })
 });
 
 client.on('message', message =>{
-	if(!message.content.startsWith(client.config["main_config"].prefix) || message.author.bot) return;
+	if(!message.content.startsWith(client.config["mainConfig"].botPrefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const cmd = args.shift().toLowerCase();
@@ -34,10 +32,10 @@ client.on('message', message =>{
 		const commandno = new Discord.MessageEmbed()
 		.setAuthor(`Command not Found in Base.`)
 		.setColor(client.config["server_config"].embed_colours)
-		.setDescription(`Run ${client.config["main_config"].prefix}order {Product} to order a product!`)
-		.setFooter(client.config["server_config"].copyright + ` | Made By HarleyDevelopment`, client.config["server_config"].server_icon)
+		.setDescription(`Run ${client.config["mainConfig"].botPrefix}order {Product} to order a product!`)
+		.setFooter(client.config["server_config"].copyright + ` | Made By HarleyDev`, client.config["server_config"].server_icon)
 		message.channel.send(commandno)
 	}
 });
 
-client.login(client.config["main_config"].token);
+client.login(client.config["mainConfig"].botToken);
