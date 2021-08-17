276898ceab44142fcd73e82c8e6483e010657c9e
H4r1eyDev <85908279+H4r1eyDev@users.noreply.github.com>
2021-08-17 18:28:12 +0100

    Update commission.js
    Better Looking commands.

--- commands/commission.js
+++ commands/commission.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor(client.config["server_config"].embed_colours)
         .setThumbnail(client.config["server_config"].server_icon)
         .setDescription(`${message.author.username} Commissions are OPEN, run ${client.config["main_config"].prefix}order {commission} to get the process started!`)
-        .setFooter(client.config["server_config"].copyright + ` | Made By HarleyDevelopment`, client.config["server_config"].server_icon)
+        .setFooter(client.config["server_config"].copyright + ` | Made By HarleyDev`, client.config["server_config"].server_icon)
         message.channel.send(status)
         }else{
             const status2 = new Discord.MessageEmbed()
@@ -15,7 +15,7 @@ module.exports = {
             .setColor(client.config["server_config"].embed_colours)
             .setThumbnail(client.config["server_config"].server_icon)
             .setDescription(`${message.author.username} Commissions are CLOSED`)
-            .setFooter(client.config["server_config"].copyright + ` | Made By HarleyDevelopment`, client.config["server_config"].server_icon)
+            .setFooter(client.config["server_config"].copyright + ` | Made By HarleyDev`, client.config["server_config"].server_icon)
             message.channel.send(status2)
         } // commission opened ending.
     }
